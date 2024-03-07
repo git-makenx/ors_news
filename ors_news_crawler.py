4b36187e45e829311e407cf4ea61ecb48e7bd271
makenx <makens@naver.com>
2024-03-07 17:25:27 +0900

    ff

--- ors_news_crawler.py
+++ ors_news_crawler.py
@@ -4,7 +4,14 @@ from datetime import timedelta, datetime
 import requests
 import pandas as pd
 import re
+
 import openpyxl
+from openpyxl.styles import Font
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+from openpyxl.styles import Alignment, PatternFill, Font, Border, Side
+import time
+import os
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 < naver 뉴스 검색시 리스트 크롤링하는 프로그램 > _select사용
 - 크롤링 해오는 것 : 링크,제목,신문사,날짜,내용요약본
@@ -13,13 +20,25 @@ import openpyxl
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 
+def makedir(dir):
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+    except OSError:
+        print("Error: Failed to create the directory.")
+
+
+
 
 #엑셀로 저장하기 위한 변수
-RESULT_PATH ='C:/Python/workspaces/ors_news/crawling_result/'  #결과 저장할 경로
+RESULT_PATH = os.getcwd() + "/" + "crawling_result/"  #결과 저장할 경로
+makedir(RESULT_PATH)
+
 now = datetime.now() #파일이름 현 시간으로 저장하기
 
 today_str = datetime.today().strftime("%Y%m%d")
 today_num = int(today_str)
+
 
 #날짜 정제화 함수
 def date_cleansing(test):
@@ -54,30 +73,32 @@ def contents_cleansing(contents):
 
 
 
-def crawler(maxpage,query,sort,s_date,e_date):
+def crawler(maxpage, query, sort, s_date, e_date):
+    # 각 크롤링 결과 저장하기 위한 리스트 선언
+    Page       = []
+    News_dates = []
+    Press_name = []
+    Title = []
+    Link = []
+    Contents = []
+    result = {}
 
     s_from = re.sub(r'[^0-9]', '', s_date)
     e_to   = re.sub(r'[^0-9]', '', e_date)
 
-    page = 1
+    page = 1  # 한페이지당 10개 기사
     maxpage_t =(int(maxpage)-1)*10+1   # 11= 2페이지 21=3페이지 31=4페이지  ...81=9페이지 , 91=10페이지, 101=11페이지
 
     while page <= maxpage_t:
         url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+sort+"&ds=" + s_date + "&de=" + e_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
 
         response = requests.get(url)
+        time.sleep(3)  # url 웹 페이지 로딩 대기 시간
+
         html = response.text
 
         #뷰티풀소프의 인자값 지정
         soup = BeautifulSoup(html, 'html.parser')
-
-        # 각 크롤링 결과 저장하기 위한 리스트 선언
-        News_dates = []
-        Press_name = []
-        Title      = []
-        Link       = []
-        Contents   = []
-        result     = {}
 
         for news_result in soup.select(".list_news > li"):
 
@@ -107,48 +128,109 @@ def crawler(maxpage,query,sort,s_date,e_date):
                 news_date = str(news_date)
                 news_date = datetime.strptime(news_date, '%Y%m%d').strftime('%Y-%m-%d')
 
+            Page.append(page)
             News_dates.append(news_date)
             Press_name.append(press_name)
             Title.append(title)
             Link.append(link)
             Contents.append(contents_cleansing(contents))
 
-            #모든 리스트 딕셔너리형태로 저장
-            result= {
-                     "기사일자"  : News_dates ,
-                     "언론사"    : Press_name ,
-                     "기사제목"  : Title ,
-                     "기사링크"  : Link ,
-                     "본문요약"  : Contents
-                     }
-
-
         print(page)
-        df = pd.DataFrame(result)  #df로 변환
         page += 10
 
+
+    #모든 리스트 딕셔너리형태로 저장
+    result= {
+             "PAGE"      : Page,
+             "기사일자"  : News_dates ,
+             "언론사"    : Press_name ,
+             "기사제목"  : Title ,
+             "기사링크"  : Link ,
+             "본문요약"  : Contents
+             }
+
+    df = pd.DataFrame(result)  # df로 변환
     # 새로 만들 파일이름 지정
     outputFileName = 'RESULT_%04d%02d%02d_%02d%02d%02d_%s.xlsx' % (now.year, now.month, now.day, now.hour, now.minute, now.second,query)
     df.to_excel(
                  RESULT_PATH + outputFileName
                , sheet_name=query
                , index=False
+               , freeze_panes=(1,0)  # 틀고정
                )
 
+    wb = load_workbook(RESULT_PATH + outputFileName)
+    ws = wb.active
+
+    # 음영 색 지정
+    ligthGrayFill = PatternFill(start_color='00C0C0C0',
+                                end_color  ='00C0C0C0',
+                                fill_type  ='solid')
+
+    ws['A1'].fill = ligthGrayFill
+    ws['B1'].fill = ligthGrayFill
+    ws['C1'].fill = ligthGrayFill
+    ws['D1'].fill = ligthGrayFill
+    ws['E1'].fill = ligthGrayFill
+    ws['F1'].fill = ligthGrayFill
+
+    ws.column_dimensions["A"].width = 8    # PAGE
+    ws.column_dimensions["B"].width = 10.25
+    ws.column_dimensions["C"].width = 20
+    ws.column_dimensions["D"].width = 50
+    ws.column_dimensions["E"].width = 50
+    ws.column_dimensions["F"].width = 200
+
+
+    # 폰트 지정
+    font_header = Font(name="맑은 고딕", size=9, bold=False)
+    for rows in ws["A1":"F500"]:
+        for cell in rows:
+            # 셀 배경색
+            #cell.fill = PatternFill(patternType="solid")
+            # 가로 위치 균등 분할
+            #cell.alignment = Alignment(horizontal="distributed")
+            # 폰트 지정
+            cell.font = font_header
+
+
+    wb.save(RESULT_PATH + outputFileName)
 
 
 
 def main():
-    #info_main = input("="*50+"\n"+"입력 형식에 맞게 입력해주세요."+"\n"+" 시작하시려면 Enter를 눌러주세요."+"\n"+"="*50)
+    # maxpage = "1"  #input("최대 크롤링할 페이지 수 입력하시오: ")
+    # query = "BNK" #input("검색어 입력: ")
+    # sort = "0"   # input("뉴스 검색 방식 입력(관련도순=0  최신순=1  오래된순=2): ")    #관련도순=0  최신순=1  오래된순=2
+    # s_date = "2024/03/01" # input("시작날짜 입력(2019.01.04):")  #2019.01.04
+    # e_date = "2024/03/07" # input("끝날짜 입력(2019.01.05):")   #2019.01.05
 
-    maxpage = "5"  #input("최대 크롤링할 페이지 수 입력하시오: ")
-    query = "BNK" #input("검색어 입력: ")
-    sort = "1"   # input("뉴스 검색 방식 입력(관련도순=0  최신순=1  오래된순=2): ")    #관련도순=0  최신순=1  오래된순=2
-    s_date = "2024/03/01" # input("시작날짜 입력(2019.01.04):")  #2019.01.04
-    e_date = "2024/03/07" # input("끝날짜 입력(2019.01.05):")   #2019.01.05
+    # 엑셀 파일 열기
+    wb = openpyxl.load_workbook(filename = os.getcwd() + "/" + "ors_news_crawler.xlsx")
 
-    crawler(maxpage,query,sort,s_date,e_date)
+    # 시트 선택하기
+    option = wb.get_sheet_by_name('OPTION')
 
+    s_date  = option["A3"].value # 검색 시작일자 YYYY.MM.DD
+    e_date  = option["B3"].value # 검색 종료일자 YYYY.MM.DD
+    maxpage = option["C3"].value # 검색 page (1page = 기사 10건)
+    sort    = option["D3"].value # 검색 옵션(정렬) 관련도순:0, 최신순:1, 오래된순:2
+
+    flag = False
+    keyword_range = option["A5":"A19"]
+    print(keyword_range)
+    for keyword in keyword_range:
+        for cell in keyword:
+            if cell.value == None:
+                flag = True
+                break
+
+            print("keyword:" + cell.value)
+            query = cell.value
+            crawler(maxpage, query, sort, s_date, e_date)
+
+        if flag == True:
+            break
 main()
 
 
